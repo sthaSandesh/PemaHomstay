2a307f5a6467c739e86c19f2dface57b7ff08067
sthaSandesh <sandeshstha561@gmail.com>
2023-09-13 14:37:17 +0545

    error 404 fix

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import AboutUs from '../component/AboutUs/AboutUs'
+import About from '../component/AboutUs/About'
+
 import Info from '../component/AboutUs/Info'
 
 export default function AboutPage() {
   return (
     <div>
-       <AboutUs />
+       <About />
        <Info />
     </div>
   )
